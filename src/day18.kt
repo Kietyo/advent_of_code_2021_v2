25716040180b9e464be9c65aeaa232931d2b9b97
Kiet <kietmsm@gmail.com>
2021-12-18 03:25:49 -0800

    Day 18. Trying to get exploding and splitting down.

--- src/day18.kt
+++ src/day18.kt
@@ -1,40 +1,159 @@
+import kotlin.math.roundToLong
+
+data class ExplosionResult(
+    val explosionOcurred: Boolean,
+    val result: SnailNode
+)
+
+sealed interface SnailNode {
+    fun print(tab: Int = 0)
+    fun split(): SnailNode
+    fun explode(depth: Int): ExplosionResult
+    operator fun plus(other: SnailNode): SnailNode
+}
+
 sealed class ParsingNode {
-    abstract fun print(tab: Int = 0): Unit
 
-    object Comma : ParsingNode() {
-        override fun print(tab: Int) {
-            print("\t".repeat(tab) + ',')
-        }
+    object Comma : ParsingNode()
+
+    object LeftBrace : ParsingNode()
+
+    object RightBrace : ParsingNode()
+}
+
+data class SnailNumber(val num: Long) : ParsingNode(), SnailNode {
+    override fun print(tab: Int) {
+        print("\t".repeat(tab) + "Number: $num")
     }
 
-    object LeftBrace : ParsingNode() {
-        override fun print(tab: Int) {
-            TODO("Not yet implemented")
-        }
+    override fun split(): SnailNode {
+        return if (num >= 10)
+            SnailPair(
+                SnailNumber(num / 2),
+                SnailNumber((num / 2.0).roundToLong())
+            )
+        else this
     }
 
-    object RightBrace : ParsingNode() {
-        override fun print(tab: Int) {
-            TODO("Not yet implemented")
-        }
+    override fun explode(depth: Int): ExplosionResult {
+        return ExplosionResult(false, this)
     }
 
-    data class Number(val num: Long) : ParsingNode() {
-        override fun print(tab: Int) {
-            print("\t".repeat(tab) + "Number: $num")
-        }
+    override fun plus(other: SnailNode): SnailPair {
+        return SnailPair(this, other)
+    }
+}
+
+data class SnailPair(val left: SnailNode, val right: SnailNode) : ParsingNode(), SnailNode {
+    fun getRightNumber(): SnailNumber {
+        require(right is SnailNumber)
+        return right as SnailNumber
     }
 
-    data class Pair(val left: ParsingNode, val right: ParsingNode) : ParsingNode() {
-        override fun print(tab: Int) {
-            println("\t".repeat(tab) + "Pair:")
-            left.print(tab + 1)
-            println()
-            right.print(tab + 1)
-            if (tab == 0) {
-                println()
+    fun getLeftNumber(): SnailNumber {
+        require(left is SnailNumber)
+        return left as SnailNumber
+    }
+
+    fun isExplodablePair(): Boolean {
+        return left is SnailNumber && right is SnailNumber
+    }
+
+    override fun explode(depth: Int): ExplosionResult {
+        if (depth >= 3) {
+            return when {
+                left is SnailPair && right is SnailNumber -> {
+                    val leftExploded = left.explode(depth + 1)
+                    if (leftExploded.explosionOcurred) {
+                        ExplosionResult(
+                            true,
+                            SnailPair(
+                                SnailNumber(0),
+                                SnailNumber(
+                                    leftExploded.getRightNumber().num + right
+                                        .num
+                                )
+                            )
+                        )
+                    } else {
+                        ExplosionResult(
+                            false,
+                        )
+                    }
+
+
+                }
+                left is SnailNumber && right is SnailPair -> {
+                    val rightExploded = right.explode(depth + 1)
+                    ExplosionResult(
+                        true,
+                        SnailPair(
+                            SnailNumber(rightExploded.getLeftNumber().num + left.num),
+                            SnailNumber(0)
+                        )
+                    )
+
+                }
+                left is SnailNumber && right is SnailNumber -> {
+                    return ExplosionResult(false, this)
+                }
+                else -> TODO(
+                    """
+                    Unhandled.
+                    left is: $left
+                    right is $right
+                """.trimIndent()
+                )
             }
         }
+
+        return when (left) {
+            is SnailNumber -> {
+                val newRight = right.explode(depth + 1)
+                if (newRight.explosionOcurred) {
+                    ExplosionResult(
+                        true,
+                        SnailPair(left, newRight.result)
+                    )
+
+                } else {
+                    ExplosionResult(
+                        false,
+                        this
+                    )
+                }
+            }
+            is SnailPair -> {
+                val newLeft = left.explode(depth + 1)
+                if (newLeft.explosionOcurred) {
+                    ExplosionResult(true, SnailPair(newLeft.result, right))
+                } else {
+                    ExplosionResult(false, this)
+                }
+            }
+        }
+
+    }
+
+    override fun print(tab: Int) {
+        println("\t".repeat(tab) + "Pair:")
+        left.print(tab + 1)
+        println()
+        right.print(tab + 1)
+        if (tab == 0) {
+            println()
+        }
+    }
+
+    override fun split(): SnailNode {
+        return SnailPair(
+            left.split(),
+            right.split()
+        )
+    }
+
+    override fun plus(other: SnailNode): SnailNode {
+        return SnailPair(this, other)
     }
 }
 
@@ -66,14 +185,14 @@ class Parser(val str: String) {
                     sb.append(str[currentIndex])
                     currentIndex++
                 }
-                ParsingNode.Number(sb.toString().toLong())
+                SnailNumber(sb.toString().toLong())
             }
             else -> TODO("Not implemented: $currChar")
         }
     }
 }
 
-fun parseSnailNode(str: String) {
+fun parseSnailNode(str: String): SnailNode {
     val stack = mutableListOf<ParsingNode>()
 
     val parser = Parser(str)
@@ -83,19 +202,19 @@ fun parseSnailNode(str: String) {
         when (currNode) {
             ParsingNode.Comma,
             ParsingNode.LeftBrace,
-            is ParsingNode.Number -> {
+            is SnailNumber -> {
                 stack.add(currNode)
             }
             ParsingNode.RightBrace -> {
-                val rightNode = stack.removeLast()
+                val rightNode = stack.removeLast() as SnailNode
                 val comma = stack.removeLast()
-                val leftNode = stack.removeLast()
+                val leftNode = stack.removeLast() as SnailNode
                 val leftBrace = stack.removeLast()
-                require(rightNode is ParsingNode.Number || rightNode is ParsingNode.Pair)
+                require(rightNode is SnailNumber || rightNode is SnailPair)
                 require(comma == ParsingNode.Comma)
-                require(leftNode is ParsingNode.Number || leftNode is ParsingNode.Pair)
+                require(leftNode is SnailNumber || leftNode is SnailPair)
                 require(leftBrace == ParsingNode.LeftBrace)
-                stack.add(ParsingNode.Pair(leftNode, rightNode))
+                stack.add(SnailPair(leftNode, rightNode))
             }
         }
 
@@ -106,8 +225,10 @@ fun parseSnailNode(str: String) {
 
     println(stack)
 
-    val rootNode = stack.first()
+    val rootNode = stack.first() as SnailNode
     rootNode.print()
+
+    return rootNode
 }
 
 fun main() {
@@ -116,17 +237,31 @@ fun main() {
         println(inputs)
         val first = inputs.first()
 
+        val root =
+            inputs.map { parseSnailNode(it) }.reduce { acc: SnailNode, snailNode: SnailNode ->
+                acc + snailNode
+            }
 
+        println(
+            """
+            root: $root
+        """.trimIndent()
+        )
 
-        for (input in inputs) {
-            val root = parseSnailNode(first)
-            println(
-                """
-                root: $root
-            """.trimIndent()
-            )
-        }
+        root.print()
 
+        var result = root
+        result = result.explode(depth + 1)
+        println("result 1:")
+        result.print()
+
+        result = result.split()
+        println("result 2:")
+        result.print()
+
+        result = result.explode(depth + 1)
+        println("result 3:")
+        result.print()
     }
 
     fun part2(inputs: List<String>) {
@@ -136,9 +271,14 @@ fun main() {
     val testInput = readInput("day18_test")
     val mainInput = readInput("day18")
 
+    val num = 23
 
-    //    part1(testInput)
-    part1(mainInput)
+    println((num / 2.0))
+    println((num / 2).toLong())
+    println((num / 2.0).roundToLong())
+
+    part1(testInput)
+    //        part1(mainInput)
     //
     //    part2(testInput)
     //    part2(mainInput)
